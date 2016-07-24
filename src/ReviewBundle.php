ce8beb34f4ed151e0b02a9a766688e1bf4728ca7
Stanislav Pivovartsev <stanislav_pivovartsev@mail.ru>
2016-07-24 23:59:29 +0300

    Make it symfony bundle.

---
+++ src/ReviewBundle.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stas
+ * Date: 24.07.16
+ * Time: 23:51.
+ */
+
+namespace StasPiv\Review;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle.
+ */
+class ReviewBundle extends Bundle
+{
+}
